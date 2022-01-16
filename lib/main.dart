597bf9e19370641748e79766e2fbb6f5b5528364
MithunVinayak <mithunvinayak10@gmail.com>
2022-01-16 15:24:16 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'price_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(/**/
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+          primaryColor: Colors.lightBlue,
+          scaffoldBackgroundColor: Colors.white),
+      home: AnimatedSplashScreen(
+        splash: Image.asset(
+          'assets/bitcoin.png',
+        ),
+        splashIconSize: 250.0,
+        nextScreen: PriceScreen(),
+      ),
+    );
+  }
+}
+//
